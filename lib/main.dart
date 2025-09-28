f42a76099a6e0315397824b7bffb3ad858d2fa6b
Walt Viviers <2278252+waltviviers@users.noreply.github.com>
2025-09-28 19:05:01 +0200

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+import 'dart:math';
 import 'package:flutter/material.dart';
 
 void main() => runApp(const NeuroTraceApp());
@@ -9,30 +11,11 @@ class NeuroTraceApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'NeuroTrace',
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData.dark(useMaterial3: true),
-      home: const _BuildOKScreen(),
-    );
-  }
-}
-
-class _BuildOKScreen extends StatelessWidget {
-  const _BuildOKScreen({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.black,
-      body: Center(
-        child: Text(
-          'NeuroTrace build OK',
-          style: TextStyle(
-            color: Colors.greenAccent.shade200,
-            fontSize: 24,
-            letterSpacing: 1.2,
-          ),
+      theme: ThemeData.dark().copyWith(
+        scaffoldBackgroundColor: Colors.black,
+        textTheme: const TextTheme(
+          headlineMedium: TextStyle(fontSize: 28, fontWeight: FontWeight.w600),
+          bodyMedium: TextStyle(fontSize: 16),
         ),
       ),
-    );
-  }
-}
+      home: const MemoryGame
